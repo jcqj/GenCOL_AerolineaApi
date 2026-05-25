745dddf1030187abe2b3cb2c3c5a9e844319aa9f
Juan Camilo Quevedo Jaimes <juanqueja64@gmail.com>
2026-05-25 00:55:00 -0500

    S1 Ok

---
+++ src/main/java/com/aerolinea/Aerolinea/controller/PasajeroController.java
@@ -0,0 +1,27 @@
+package com.aerolinea.Aerolinea.controller;
+
+import com.aerolinea.Aerolinea.model.Pasajero;
+import com.aerolinea.Aerolinea.service.PasajeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pasajeros")
+public class PasajeroController {
+    private final PasajeroService pasajeroService;
+
+    @Autowired
+    public PasajeroController(PasajeroService pasajeroService) {
+        this.pasajeroService = pasajeroService;
+    }
+
+    @GetMapping
+    public List<Pasajero> obtenerTodos() {
+        return pasajeroService.findAll();
+    }
+
+}
--- src/main/java/com/aerolinea/Aerolinea/controller/VueloController.java
+++ src/main/java/com/aerolinea/Aerolinea/controller/VueloController.java
@@ -1,6 +1,24 @@
 package com.aerolinea.Aerolinea.controller;
 
-public class VueloController
-{
+import com.aerolinea.Aerolinea.model.Vuelo;
+import com.aerolinea.Aerolinea.service.VueloService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/vuelos")
+public class VueloController {
+
+    private final VueloService vueloService;
+
+    public VueloController(VueloService vueloService) {
+        this.vueloService = vueloService;
+    }
+    @GetMapping
+    public List<Vuelo> obtenerTodos() {
+        return vueloService.findAll();
+    }
 }
